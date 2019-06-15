95fa7f64568151fabd47db00ae58d556626a044a
Atharva Khare <khareatharva@gmail.com>
2019-06-15 21:37:58 +0530

    asda

--- levelUp/settings.py
+++ levelUp/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
