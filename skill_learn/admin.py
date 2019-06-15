c110f31df7de61612d4518eda5344636502557fa
Atharva Khare <khareatharva@gmail.com>
2019-06-15 11:54:19 +0530

    Updated models and added string aliases

--- skill_learn/admin.py
+++ skill_learn/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from skill_learn.models import *
+
+admin.site.register(Student)
+admin.site.register(Topic)
+admin.site.register(UserRating)
+admin.site.register(Text)
+admin.site.register(Question)
 
 # Register your models here.
--- skill_learn/models.py
+++ skill_learn/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    username = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
+
+
+class Topic(models.Model):
+    name = models.TextField(blank=True, null=True)
+    rating = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class UserRating(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    rating = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.student.username + "(" + self.topic + "): " + str(self.rating)
+
+
+class Text(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    identifier = models.CharField(max_length=200, blank=True, null=True)
+    text = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.topic.name + ": " + self.identifier
+
+
+class Question(models.Model):
+    summary_text = models.ForeignKey(Text, null=True, blank=True, on_delete=models.CASCADE, related_name='summary_text')
+    original_text = models.ForeignKey(Text, null=True, blank=True, on_delete=models.CASCADE, related_name='original_text')
+    question = models.TextField(blank=True, null=True)
+    option1 = models.TextField(blank=True, null=True)
+    option2 = models.TextField(blank=True, null=True)
+    option3 = models.TextField(blank=True, null=True)
+    option4 = models.TextField(blank=True, null=True)
+    correct = models.TextField(blank=True, null=True)
+    rating = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.question
